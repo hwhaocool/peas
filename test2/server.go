b286d27e59093bc04155d62ec1dce6e56ad94ed1
YellowTail <hwhaocool@gmail.com>
2019-08-10 17:33:47 +0800

    提交来自 https://github.com/deviantony/gosrv/ 的代码

---
+++ test2/server.go
@@ -0,0 +1,99 @@
+package main
+
+// import "github.com/deviantony/gosrv"
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	port = "7777"
+)
+
+var clients []net.Conn
+
+func main() {
+	l, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	log.Print("Server listening...")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err)
+		}
+
+		log.Printf("Client connected %s -> %s \n", conn.RemoteAddr(), conn.LocalAddr())
+		// Add the client to the connection array
+		clients = append(clients, conn)
+
+		go handler(conn)
+
+		// go input()
+	}
+}
+
+func removeClient(conn net.Conn) {
+	log.Printf("Client %s disconnected", conn.RemoteAddr())
+	conn.Close()
+	//remove client from clients here
+}
+
+func handler(conn net.Conn) {
+	defer removeClient(conn)
+	errorChan := make(chan error)
+	dataChan := make(chan []byte)
+
+	go readWrapper(conn, dataChan, errorChan)
+
+	for {
+		select {
+		case data := <-dataChan:
+			log.Printf("Client %s sent: %s", conn.RemoteAddr(), string(data))
+			for i := range clients {
+				clients[i].Write(data)
+			}
+		case err := <-errorChan:
+			log.Println("An error occured:", err.Error())
+			return
+		}
+	}
+}
+
+func readWrapper(conn net.Conn, dataChan chan []byte, errorChan chan error) {
+	for {
+		// buf := make([]byte, 1024)
+		// reqLen, err := conn.Read(buf)
+		// if err != nil {
+		// 	errorChan <- err
+		// 	return
+		// }
+		// dataChan <- buf[:reqLen]
+
+		input(dataChan)
+	}
+}
+
+func input(dataChan chan []byte) {
+	// for {
+	fmt.Print(">")
+
+	// read in input from stdin
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+
+	dataChan <- []byte(text)
+
+	// for _, conn := range clients {
+	// 	// send to socket
+	// 	fmt.Fprintf(conn, text)
+	// }
+	// }
+}
